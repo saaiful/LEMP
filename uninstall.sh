d5c509e6c6b326f4e54a7dad9d30b8aa0784b955
Saiful Islam <saaifulislam@gmail.com>
2017-05-08 00:07:41 +0600

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,13 +1,13 @@
 # Remove PHP
-sudo apt-get -y purge php.*
+sudo apt-get -y php.*
 sudo apt-get -y autoremove
 sudo apt-get -y autoclean
 # Remove Nginx
-sudo apt-get remove -purge nginx nginx-common
+sudo apt-get remove nginx nginx-common
 sudo apt-get -y autoremove
 sudo apt-get -y autoclean
 # Remove MySQL
-sudo apt-get -y remove --purge mysql-server mysql-client mysql-commo
+sudo apt-get -y remove mysql-server mysql-client mysql-commo
 sudo apt-get -y autoremove
 sudo apt-get -y autoclean
 sudo rm -rf /var/lib/mysql
