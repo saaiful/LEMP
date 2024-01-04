17a861abd53cd6ff9ba9ed29d7f75a5763933bbe
Saiful Islam <saaifulislam@gmail.com>
2024-01-04 20:46:42 +0600

    Create node18.sh

---
+++ node18.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Install Node.js 18.x
+sudo mkdir -p /etc/apt/keyrings
+sudo rm -f /etc/apt/keyrings/nodesource.gpg && curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+NODE_MAJOR=18
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+sudo apt-get update -y
+sudo apt-get install -y nodejs
